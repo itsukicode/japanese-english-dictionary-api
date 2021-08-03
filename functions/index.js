23c3a045f6990365c7818b44199821575040aff3
tommytommychopper <t.itsuki928@gmail.com>
2021-08-02 20:29:08 -0700

    [add] express app

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
+const express = require('express')
+const app = express()
+const scrapeWord = require('./scrapeWord')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.get('/search-word', async (req, res) => {
+	const searchWord = req.query.word
+	try {
+		const word = await scrapeWord(searchWord)
+		res.status(200).send({
+			word
+		})
+	} catch (e) {
+		res.status(404).send({
+			message: 'Sorry there is not such a word in our database'
+		})
+	}
+})
+exports.app = functions.https.onRequest(app)

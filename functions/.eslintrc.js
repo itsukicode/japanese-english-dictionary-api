d3ae9d14782b117eeca0e4910036d08c38308f22
tommytommychopper <t.itsuki928@gmail.com>
2021-08-02 20:24:35 -0700

    [fix] some configlike semicolon

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
-};
+	'root': true,
+	'env': {
+		'es6': true,
+		'node': true
+	},
+	'extends': ['eslint:recommended', 'google'],
+	'rules': {
+		'quotes': ['error', 'single'],
+		'indent': [2, 'tab'],
+		'semi': ['error', 'never'],
+		'no-tabs': 0,
+		'comma-dangle': ['error', 'never'],
+		'max-len': ['error', {'code': 200}]
+	},
+	'parserOptions': {
+		'ecmaVersion': 2017
+	}
+}

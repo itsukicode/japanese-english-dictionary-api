d48aeff401e6f25ce289e422d4197304966cd1b8
tommytommychopper <t.itsuki928@gmail.com>
2021-08-11 02:56:36 -0700

    [optimize] jp func

--- functions/scrapeWord.js
+++ functions/scrapeWord.js
@@ -12,27 +12,20 @@ async function jp(word) {
 		await page.goto(`https://www.google.com/search?q=${word}+意味`, {
 			waitUntil: 'domcontentloaded'
 		})
+		await page.waitForTimeout(1000)
 		// Get Word Pronunciation
-		await page.waitForSelector('[data-dobid="hdw"]', {timeout: 10000})
-		const [pronounceEl] = await page.$x(
-			'/html/body/div[7]/div/div[8]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/span/div/div/div[1]/div/div[2]/div[1]/div/span'
+		const pronounce = await page.$eval(
+			'[data-dobid="hdw"]',
+			(el) => el.textContent
 		)
-		let pronounce = await pronounceEl.getProperty('textContent')
-		pronounce = await pronounce.jsonValue()
-		// Get Word Audio Src
-		await page.waitForSelector('[jsname="QInZvb"] source', {timeout: 10000})
-		const [audioSrcEl] = await page.$x(
-			'/html/body/div[7]/div/div[8]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/span/div/div/div[1]/div/div[1]/div/audio/source'
+		const audioSrc = await page.$eval('audio source', (el) =>
+			el.getAttribute('src')
 		)
-		let audioSrc = await audioSrcEl.getProperty('src')
-		audioSrc = await audioSrc.jsonValue()
-		// // Get Word Definition
-		await page.waitForSelector('[data-dobid="dfn"] span', {timeout: 10000})
-		const [defEl] = await page.$x(
-			'/html/body/div[7]/div/div[8]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/span/div/div/div[1]/div/div[4]/div/div/ol/li/div/div/div[1]/div/div/div[1]/span'
+		// Get Word Definition
+		const def = await page.$eval(
+			'[data-dobid="dfn"] span',
+			(el) => el.textContent
 		)
-		let def = await defEl.getProperty('textContent')
-		def = await def.jsonValue()
 		return {pronounce, audioSrc, def}
 	} catch (err) {
 		throw new Error('取得に失敗しました。')
@@ -68,7 +61,7 @@ async function en(word) {
 		)
 		return {pronounce, audioSrc, def}
 	} catch (err) {
-		throw new Error("couldn't retrieve the data")
+		throw new Error("Couldn't retrieve the data")
 	} finally {
 		// Close Browser
 		await browser.close()

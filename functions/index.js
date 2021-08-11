f31cd3b60c65712b8c5509d6be55e289e5ba0231
tommytommychopper <t.itsuki928@gmail.com>
2021-08-11 02:17:13 -0700

    [add] selection of language

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const scrapeWord = require('./scrapeWord')
 const cors = require('cors')
+
 app.use(
 	cors({
 		origin: '*'
@@ -10,15 +11,23 @@ app.use(
 )
 
 app.get('/search-word', async (req, res) => {
+	const lang = req.query.lang
 	const searchWord = req.query.word
 	try {
-		const word = await scrapeWord(searchWord)
+		let word = null
+		if (lang === 'jp') {
+			word = await scrapeWord.jp(searchWord)
+		} else if (lang === 'en') {
+			word = await scrapeWord.en(searchWord)
+		} else {
+			throw new Error(`${lang} is not supported`)
+		}
 		res.status(200).send({
 			word
 		})
 	} catch (e) {
 		res.status(404).send({
-			message: 'Sorry there is not such a word in our database'
+			message: e.message
 		})
 	}
 })

37c665bbddf922eec70bd4f6ba3ed42e4a1bad19
tommytommychopper <t.itsuki928@gmail.com>
2021-08-13 19:35:54 -0700

    [add] return language parameter

--- functions/index.js
+++ functions/index.js
@@ -13,14 +13,25 @@ app.use(
 app.get('/search-word', async (req, res) => {
 	const lang = req.query.lang
 	const searchWord = req.query.word
+	const returnLang = req.query.returnLang
 	try {
 		let word = null
-		if (lang === 'jp') {
-			word = await scrapeWord.jp(searchWord)
-		} else if (lang === 'en') {
-			word = await scrapeWord.enToJp(searchWord)
+		if (returnLang === 'en') {
+			if (lang === 'jp') {
+				word = await scrapeWord.jpToEn(searchWord)
+			} else if (lang === 'en') {
+				word = await scrapeWord.en(searchWord)
+			} else {
+				throw new Error(`${lang} is not supported`)
+			}
 		} else {
-			throw new Error(`${lang} is not supported`)
+			if (lang === 'jp') {
+				word = await scrapeWord.jp(searchWord)
+			} else if (lang === 'en') {
+				word = await scrapeWord.enToJp(searchWord)
+			} else {
+				throw new Error(`${lang} is not supported`)
+			}
 		}
 		res.status(200).send({
 			word
